f15af2e2d765b0ab0a437c4d5cbcd202acc611ce
Alexander <alexandergrundmann98@gmail.com>
2017-02-24 16:35:47 +0100

    test2

--- DragonsEdgeRefactored/src/net/kleopi/Tests/StartBoth.java
+++ DragonsEdgeRefactored/src/net/kleopi/Tests/StartBoth.java
@@ -4,6 +4,7 @@ import net.kleopi.Client.Main.ClientMain;
 import net.kleopi.Server.MainServer;
 
 public class StartBoth {
+	// nother comment
 	public static void main(String args[]) {
 
 		ClientMain.main(null);
