1ad27cabf02d98364f62213135c0b9bb8045ebc8
Alexander <alexandergrundmann98@gmail.com>
2017-02-24 16:46:04 +0100

    removed uneccessary files

--- DragonsEdgeRefactored/src/net/kleopi/Tests/StartBoth.java
+++ DragonsEdgeRefactored/src/net/kleopi/Tests/StartBoth.java
@@ -4,7 +4,6 @@ import net.kleopi.Client.Main.ClientMain;
 import net.kleopi.Server.MainServer;
 
 public class StartBoth {
-	// comment it back
 	public static void main(String args[]) {
 
 		ClientMain.main(null);
